3e7c2810ce539f72e57a3deb99f1a489d0bfaafc
puseyr <pusey.ronald@gmail.com>
2022-05-15 21:42:29 -0400

    Adding PS1 Script

---
+++ MBOX Check.ps1
@@ -0,0 +1,12 @@
+﻿$UserCredential = Get-Credential
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://exchange.server.net/PowerShell/ -Authentication Kerberos -Credential $UserCredential
+Import-PSSession $Session
+
+[void][Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic')
+
+$title = 'User E-Mail'
+$msg   = 'Enter user E-Mail:'
+
+$email = [Microsoft.VisualBasic.Interaction]::InputBox($msg, $title)
+
+get-mailbox $email | FL defaultPublicFolderMailbox
